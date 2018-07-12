b8a4a936c508ed88c03e160b4e94f49c67560f26
Evan Travers <evantravers@gmail.com>
2018-07-12 11:08:28 -0500

    Adding a rough game pad mode

--- keyboards/planck/keymaps/evantravers/keymap.c
+++ keyboards/planck/keymaps/evantravers/keymap.c
@@ -36,6 +36,7 @@ enum planck_layers {
   _LOWER,
   _RAISE,
   _SKETCH,
+  _GAMING,
   _ADJUST
 };
 
@@ -62,8 +63,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = {
     {KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
     {CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, TD(TD_SHIFT_ENTER)},
+    {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SFT_ENTER},
     {KC_F19,   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_SPC,  RAISE, KC_RGUI, KC_RALT, KC_RCTL, KC_ENT}
+  },
+
+  /* Gaming
+   * ,-----------------------------------------------------------------------------------.
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |      |      |      |      |      | Leave|
+   * |------+------+------+------+------+-------------+------+------+------+------+------|
+   * | Ctrl |   Q  |   W  |   E  |   R  |   I  |      |      |      |      |      |      |
+   * |------+------+------+------+------+------|------+------+------+------+------+------|
+   * | Shift|   A  |   S  |   D  |   F  | Esc  |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+------+------+------+------+------+------|
+   * | Enter|   Z  |   X  |   C  |   V  | Space|      |      |      |      |      |      |
+   * `-----------------------------------------------------------------------------------'
+   */
+  [_GAMING] = {
+    {KC_TAB,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(_QWERTY)},
+    {CTRL_ESC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_I,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+    {KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F,  KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
+    {KC_ENT,   KC_Z,  KC_X,  KC_C,  KC_V,  KC_SPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO}
 },
 
 /* Sketch
@@ -138,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,     RGB_VAI, RGB_VAD, KC_DEL },
+  {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,     RGB_VAI, RGB_VAD, TO(_GAMING)},
   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, TG(_SKETCH), _______, _______, _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF,    _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______}
@@ -159,6 +178,4 @@ bool music_mask_user(uint16_t keycode) {
     default:
       return true;
   }
-}
-
-
+};
