6af6e9f19ba7658de0f571c87601626a12a11465
Evan Travers <evantravers@gmail.com>
2018-07-17 16:26:32 -0500

    Update Sketch Layer
    Using the tab key as a layer toggle is fairly inspired and I'm really
    happy with it.

--- keyboards/planck/keymaps/evantravers/keymap.c
+++ keyboards/planck/keymaps/evantravers/keymap.c
@@ -17,10 +17,11 @@
 #include "planck.h"
 #include "action_layer.h"
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define CTRL_ESC TD(TD_CTRL_ESC)
+#define LOWER     MO(_LOWER)
+#define RAISE     MO(_RAISE)
+#define CTRL_ESC  TD(TD_CTRL_ESC)
 #define SFT_ENTER TD(TD_SHIFT_ENTER)
+#define TAB_SK    LT(_SKETCH, KC_TAB)
 
 extern keymap_config_t keymap_config;
 
@@ -61,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = {
-    {KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
+    {TAB_SK,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
     {CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT},
     {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SFT_ENTER},
     {KC_F19,   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_SPC,  RAISE, KC_RGUI, KC_RALT, KC_RCTL, KC_ENT}
@@ -157,10 +158,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,     RGB_VAI, RGB_VAD, TO(_GAMING)},
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, TG(_SKETCH), _______, _______, _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF,    _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______}
+  {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, TO(_GAMING)},
+  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______,  _______, _______, _______},
+  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______}
 }
 
 
