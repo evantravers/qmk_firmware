426024b64a40283876f67d52967eb45f4850d971
Evan Travers <evantravers@gmail.com>
2018-07-27 13:35:18 -0500

    Switch to using the Space Cadet Enter
    This version is way better than the version I had implemented, I'm going
    to give a whirl.
    
    https://docs.qmk.fm/#/feature_space_shift_cadet

--- keyboards/planck/keymaps/evantravers/keymap.c
+++ keyboards/planck/keymaps/evantravers/keymap.c
@@ -20,7 +20,6 @@
 #define LOWER     MO(_LOWER)
 #define RAISE     MO(_RAISE)
 #define CTRL_ESC  TD(TD_CTRL_ESC)
-#define SFT_ENTER TD(TD_SHIFT_ENTER)
 #define TAB_SK    LT(_SKETCH, KC_TAB)
 #define SCRNSHT   LSFT(LGUI(KC_4))
 #define ZOOMIN    LGUI(KC_PLUS)
@@ -44,7 +43,6 @@ enum planck_layers {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_CTRL_ESC]    = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_ESC),
-  [TD_SHIFT_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_ENT)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -63,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = {
     {TAB_SK,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
     {CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SFT_ENTER},
-    {KC_F19,   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_SPC,  RAISE, KC_RGUI, KC_RALT, KC_RCTL, KC_ENT}
+    {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT},
+    {KC_F19,   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_SPC,  RAISE, KC_RGUI, KC_RALT, KC_RCTL, KC_F19}
   },
 
   /* Gaming
