f8ee5990edace65aa8a5456b75425f6b58f708db
Evan Travers <evantravers@gmail.com>
2018-08-20 15:54:33 -0500

    Update layout to use split-friendly layout func
    Thanks @drashna!

--- layouts/community/ortho_4x12/fullmods/keymap.c
+++ layouts/community/ortho_4x12/fullmods/keymap.c
@@ -57,12 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | F19  | Ctrl | Alt  | GUI  |Lower | Space| Space|Raise | GUI  | Alt  | Ctrl | Enter|
    * `-----------------------------------------------------------------------------------'
    */
-  [_QWERTY] = {
-    {TAB_SK,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
-    {CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-    {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT},
-    {KC_F19,   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_SPC,  RAISE, KC_RGUI, KC_RALT, KC_RCTL, KC_F19}
-  },
+  [_QWERTY] = LAYOUT_ortho_4x12(
+    TAB_SK,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+    CTRL_ESC, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+    KC_F19,   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_SPC,  RAISE, KC_RGUI, KC_RALT, KC_RCTL, KC_F19
+  ),
 
   /* Gaming
    * ,-----------------------------------------------------------------------------------.
@@ -75,12 +75,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Enter|   Z  |   X  |   C  |   V  | Space|      |      |      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
-  [_GAMING] = {
-    {KC_TAB,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(_QWERTY)},
-    {CTRL_ESC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_I,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-    {KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F,  KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO},
-    {KC_ENT,   KC_Z,  KC_X,  KC_C,  KC_V,  KC_SPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO}
-},
+  [_GAMING] = LAYOUT_ortho_4x12(
+    KC_TAB,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(_QWERTY),
+    CTRL_ESC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_I,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F,  KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_ENT,   KC_Z,  KC_X,  KC_C,  KC_V,  KC_SPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+),
 
 /* Sketch
  * This is an attempt to make some of the shortcuts I use in Sketch.app easier
@@ -99,12 +99,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_SKETCH] = {
-  {_______, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), _______, _______, KC_UP,   _______,  _______, _______},
-  {_______, _______,    _______,    ZOOMOUT,    ZOOMIN,     _______,    _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______},
-  {_______, _______,    _______,    _______,    _______,    _______,    _______, _______, _______, _______,  _______, _______},
-  {_______, _______,    _______,    _______,    _______,    _______,    _______, _______, _______, _______,  _______, _______}
-},
+[_SKETCH] = LAYOUT_ortho_4x12(
+  _______, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), _______, _______, KC_UP,   _______,  _______, _______,
+  _______, _______,    _______,    ZOOMOUT,    ZOOMIN,     _______,    _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
+  _______, _______,    _______,    _______,    _______,    _______,    _______, _______, _______, _______,  _______, _______,
+  _______, _______,    _______,    _______,    _______,    _______,    _______, _______, _______, _______,  _______, _______
+),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -117,12 +117,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
-},
+[_LOWER] = LAYOUT_ortho_4x12(
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
+  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -135,12 +135,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = {
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY}
-},
+[_RAISE] = LAYOUT_ortho_4x12(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_BSLS,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY
+),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
@@ -153,12 +153,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = {
-  {_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, TO(_GAMING)},
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______,  _______, _______, _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______}
-}
+[_ADJUST] = LAYOUT_ortho_4x12(
+  _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, TO(_GAMING),
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______,  _______, _______, _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+)
 
 
 };
