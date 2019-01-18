1e6dda26913244e1286d0543c7700a7452297596
Evan Travers <evantravers@gmail.com>
2019-01-18 14:08:27 -0600

    Attempt to move my layout out to shared vars
    Borrowing heavily from @drashna's fine work.

---
+++ users/evantravers/evantravers.h
@@ -0,0 +1,53 @@
+/*
+Copyright 2019 Evan Travers <evantravers@gmail.com> @evantravers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "quantum.h"
+#include "version.h"
+#include "eeprom.h"
+#include "wrappers.h"
+
+#define LOWER     MO(_LOWER)
+#define RAISE     MO(_RAISE)
+#define SHORTCUTS MO(_SHORTCUTS)
+#define CTRL_ESC  TD(TD_CTRL_ESC)
+#define TAB_SK    LT(_SHORTCUTS, KC_TAB)
+#define GAME_TAB  LT(_GAMING_SHORTCUTS, KC_TAB)
+#define ZOOMIN    LGUI(KC_PLUS)
+#define ZOOMOUT   LGUI(KC_MINS)
+#define SW_WIND   LGUI(KC_GRV)
+#define SCRNST    LGUI(LSFT(LCTL(KC_4)))
+
+enum {
+  TD_CTRL_ESC
+};
+
+/* Define layer names */
+enum userspace_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _SHORTCUTS,
+  _GAMING,
+  _GAMING_SHORTCUTS,
+  _ADJUST
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_CTRL_ESC]    = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_ESC),
+};
+
