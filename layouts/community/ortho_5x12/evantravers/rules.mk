79fed6ef7b359ef908326d2b0fc180c1337b2c2b
Evan Travers <evantravers@gmail.com>
2019-01-19 09:22:29 -0600

    Clean up the preonic map
    Trying to remove dead code.

--- layouts/community/ortho_5x12/evantravers/rules.mk
+++ layouts/community/ortho_5x12/evantravers/rules.mk
@@ -5,13 +5,6 @@ CONSOLE_ENABLE    = no         # Console for debug(+400)
 COMMAND_ENABLE    = no        # Commands for debug and configuration
 TAP_DANCE_ENABLE  = yes
 AUDIO_ENABLE      = yes
-ifneq (,$(findstring fractal,$(KEYBOARD))) # Make sure it IS the Planck Light
-  RGB_MATRIX_ENABLE   = no
-  AUDIO_ENABLE        = no
-  RGBLIGHT_ENABLE     = yes
-  RGBLIGHT_TWINKLE    = yes
-  BOOTLOADER          = qmk-dfu
-endif
 
 ifeq ($(strip $(PROTOCOL)), VUSB)
 NKRO_ENABLE       = no
