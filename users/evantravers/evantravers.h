7d1fe9cc6dc26bdc9be23b376cd7ec3e6d6bf5c5
Evan Travers <evantravers@gmail.com>
2020-03-06 18:36:33 -0600

    Remove GAMING stuff
    Not only have I not used it in six months, but the default shortcuts
    layer is _just_ the numbers anyway, which was the whole point of the
    gaming shortcut layers anyway.

--- users/evantravers/evantravers.h
+++ users/evantravers/evantravers.h
@@ -27,7 +27,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SHORTCUTS MO(_SHORTCUTS)
 #define CTRL_ESC  TD(TD_CTRL_ESC)
 #define TAB_SK    LT(_SHORTCUTS, KC_TAB)
-#define TAB_GAM   LT(_GAMING_SHORTCUTS, KC_TAB)
 #define ZOOMIN    LGUI(KC_PLUS)
 #define ZOOMOUT   LGUI(KC_MINS)
 #define TABN      LSFT(LGUI(KC_LBRC))
@@ -52,10 +51,8 @@ enum userspace_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _GAMING,
   _ADJUST,
-  _SHORTCUTS,
-  _GAMING_SHORTCUTS
+  _SHORTCUTS
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
