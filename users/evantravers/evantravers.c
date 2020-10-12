311772ca9993dc22e42c2f9fe89dbfefc1d3e5f6
Evan Travers <evantravers@gmail.com>
2020-10-12 16:44:30 -0500

    Fix my build system
    Drashna pointed out that my rules.mk should be including my `.c` file in
    userspace. This is why no code that I put in my `.c` file ever ran.
    After this I had to change a few other things.
    
    Ridingqwerty showed me that I should be using some safe ranges for my
    enums.

--- users/evantravers/evantravers.c
+++ users/evantravers/evantravers.c
@@ -16,3 +16,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "evantravers.h"
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_CTRL_ESC]    = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_ESC),
+  [TD_PREV_NEXT]   = ACTION_TAP_DANCE_DOUBLE(KC_MFFD, KC_MRWD),
+  [TD_SCRNST]      = ACTION_TAP_DANCE_DOUBLE(LGUI(LSFT(LCTL(KC_4))), LGUI(LSFT(KC_4)))
+};
