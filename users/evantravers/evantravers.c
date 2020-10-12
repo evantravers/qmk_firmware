9ead76d841ad6c65466dfd77057c6092d1e74328
Evan Travers <evantravers@gmail.com>
2020-10-12 16:51:55 -0500

    Move F19 to 3x5_3 wrappers
    I've been trying to figure out how to do this for a while. Ridingqwerty
    helped write most of the code, Drashna added the `get_tapping_term`
    magic.

--- users/evantravers/evantravers.c
+++ users/evantravers/evantravers.c
@@ -17,8 +17,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "evantravers.h"
 
+uint32_t user_key_timer;
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record);
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_CTRL_ESC]    = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_ESC),
   [TD_PREV_NEXT]   = ACTION_TAP_DANCE_DOUBLE(KC_MFFD, KC_MRWD),
   [TD_SCRNST]      = ACTION_TAP_DANCE_DOUBLE(LGUI(LSFT(LCTL(KC_4))), LGUI(LSFT(KC_4)))
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case ESC_F19:
+            if (record->event.pressed) {
+                user_key_timer = timer_read();
+                register_code(KC_F19);
+            } else {
+                unregister_code(KC_F19);
+                if (timer_elapsed(user_key_timer) < get_tapping_term(keycode, record)) {
+                    tap_code(KC_ESC);
+                }
+            }
+            return false; break;
+        default:
+            return true; // Process all other keycodes normally
+    }
+}
