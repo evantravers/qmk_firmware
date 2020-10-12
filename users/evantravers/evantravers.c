ad16ea454e8340826b8b191fdb3fd94cf5252c00
Evan Travers <evantravers@gmail.com>
2020-10-12 16:52:48 -0500

    Move trilayer function to userspace
    There's not a really good reason for this other than I feel good DRYing
    things up.

--- users/evantravers/evantravers.c
+++ users/evantravers/evantravers.c
@@ -43,3 +43,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; // Process all other keycodes normally
     }
 }
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
