6c694f322b2e75088293616e476ea60dd80c1c90
Evan Travers <evantravers@gmail.com>
2020-10-12 16:52:48 -0500

    Move trilayer function to userspace
    There's not a really good reason for this other than I feel good DRYing
    things up.

--- layouts/community/ortho_4x12/evantravers/keymap.c
+++ layouts/community/ortho_4x12/evantravers/keymap.c
@@ -67,10 +67,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-uint32_t layer_state_set_user(uint32_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
 bool music_mask_user(uint16_t keycode) {
   switch (keycode) {
     case RAISE:
