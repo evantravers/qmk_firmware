f43ced026af0b3d087c5fad25ac5361d91135878
Evan Travers <evantravers@gmail.com>
2020-12-11 10:24:16 -0600

    Move KC_COLN, KC_BSLS
    I think the `:` thing feels quite good, and it makes sense to have the
    slash above the other slash... `/` `\`, but I can't decide what should
    be up there above `p`. It just feels weird.

--- users/evantravers/wrappers.h
+++ users/evantravers/wrappers.h
@@ -38,9 +38,9 @@
 #define _________________LOWER_L2__________________        KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV
 #define _________________LOWER_L3__________________        KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD
 
-#define _________________LOWER_R1__________________        KC_CIRC, KC_UNDS, KC_PLUS, KC_SLSH, KC_BSLS
-#define _________________LOWER_R2__________________        KC_SLSH, KC_MINS, KC_EQL,  KC_ASTR, _______
-#define _________________LOWER_R3__________________        KC_AMPR, KC_QUES, KC_LT,   KC_GT,   KC_COLN
+#define _________________LOWER_R1__________________        KC_CIRC, KC_UNDS, KC_PLUS, KC_SLSH, _______
+#define _________________LOWER_R2__________________        KC_SLSH, KC_MINS, KC_EQL,  KC_ASTR, KC_COLN
+#define _________________LOWER_R3__________________        KC_AMPR, KC_QUES, KC_LT,   KC_GT,   KC_BSLS
 
 
 
