e273b039788af202ca8b4055b79e1a5e4f5a0301
Evan Travers <evantravers@gmail.com>
2021-01-08 10:10:29 -0600

    Rename layers
    I'm not really using RAISE and LOWER the same way...

--- users/evantravers/evantravers.h
+++ users/evantravers/evantravers.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "eeprom.h"
 #include "wrappers.h"
 
-#define LOWER     LT(_LOWER, KC_BSPC)
-#define RAISE     LT(_RAISE, KC_SPC)
+#define SYMBL     LT(_SYMBL, KC_BSPC)
+#define CNTRL     LT(_CNTRL, KC_SPC)
 #define SHORTCUTS MO(_SHORTCUTS)
 #define CTRL_ESC  TD(TD_CTRL_ESC)
 #define TAB_SK    LT(_SHORTCUTS, KC_TAB)
@@ -58,8 +58,8 @@ enum userspace_custom_keycodes {
 /* Define layer names */
 enum userspace_layers {
   _QWERTY,
-  _LOWER,
-  _RAISE,
+  _SYMBL,
+  _CNTRL,
   _ADJUST,
   _SHORTCUTS
 };
