f50f061e9e6241e54fa930d6065c632e0e5573b2
Evan Travers <evantravers@gmail.com>
2022-04-03 14:39:14 -0500

    Swap GUI with the layers

--- keyboards/preonic/keymaps/evantravers/keymap.c
+++ keyboards/preonic/keymaps/evantravers/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TAB_SK,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LCTL, SYMBL,   KC_LGUI, KC_LALT,          KC_SPC,  KC_SPC,           KC_RALT, KC_RGUI, CNTRL,   KC_RCTL
+  KC_LCTL, KC_LGUI, SYMBL,   KC_LALT,          KC_SPC,  KC_SPC,           KC_RALT, CNTRL,   KC_RGUI, KC_RCTL
 ),
 
 [_QWERTY] = LAYOUT_preonic_2x2u_wrapper(
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TAB_SK,  _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_BSPC,
   KC_LCTL, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, KC_QUOT,
   KC_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_________________, KC_SFTENT,
-  KC_LCTL, SYMBL,   KC_LGUI, ESC_F19,          SYMBL,   CNTRL,            QUOTE_SK,KC_RGUI, CNTRL,   KC_RCTL
+  KC_LCTL, KC_LGUI, SYMBL,  ESC_F19,           SYMBL,   CNTRL,            QUOTE_SK,CNTRL,   KC_RGUI, KC_RCTL
 ),
 
 [_SYMBL] = LAYOUT_preonic_2x2u_wrapper(
