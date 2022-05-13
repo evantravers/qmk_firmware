625a57745154cfce841b6b36f6d6b4f0ea4bf486
Evan Travers <evantravers@gmail.com>
2022-05-13 17:03:04 -0500

    Attempting to parrot clever speed hax
    Thank you @stapelberg.
    
    References:
    - https://github.com/qmk/qmk_firmware/pull/12626
    - https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/#quickusbpolling

--- users/evantravers/rules.mk
+++ users/evantravers/rules.mk
@@ -9,9 +9,11 @@ else
   NKRO_ENABLE       = yes
 endif
 
-BOOTMAGIC_ENABLE  = no  # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE   = no  # Mouse keys(+4700)
-EXTRAKEY_ENABLE   = yes # Audio control and System control(+450)
-CONSOLE_ENABLE    = no  # Console for debug(+400)
-COMMAND_ENABLE    = no  # Commands for debug and configuration
-MACROS_ENABLED    = no
+BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE  = no  # Mouse keys(+4700)
+EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
+CONSOLE_ENABLE   = no  # Console for debug(+400)
+COMMAND_ENABLE   = no  # Commands for debug and configuration
+MACROS_ENABLED   = no
+
+DEBOUNCE_TYPE    = sym_eager_pk
